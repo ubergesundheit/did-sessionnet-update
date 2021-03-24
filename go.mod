a6d9e02c57990e8f2c02151f6fd0b289b620b536
Gerald Pape <mail@geraldpape.io>
2021-03-24 09:45:51 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module git.vo1d.space/gerald/scrape-sessionnet
+
+go 1.16
+
+require github.com/gocolly/colly/v2 v2.1.0
---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	colly "github.com/gocolly/colly/v2"
+)
+
+// 2021-03-23:
+// Somacos GmbH & Co. KG,https://www.somacos.de, SessionNet Version 5.1.8 bi (Layout 5)
+// Somacos GmbH & Co. KG,https://www.somacos.de, SessionNet Version 5.2.3 KP3 bi (Layout 5)
+// Version changed (from/to)
+// V:050108
+// V:050203
+
+
+var expectedAuthor = "Somacos GmbH & Co. KG,https://www.somacos.de, SessionNet Version 5.2.3 KP3 bi (Layout 5)"
+var expectedVersion = "V:050203"
+
+func checkVersion() {
+	url := "https://www.stadt-muenster.de/sessionnet/sessionnetbi/info.php"
+	exitCode := 0
+
+	c := colly.NewCollector()
+
+	c.OnHTML("meta[name=author]", func(e *colly.HTMLElement) {
+		newAuthor := e.Attr("content")
+
+		if newAuthor != expectedAuthor {
+			fmt.Printf("Author changed (from/to)\n%s\n%s\n", expectedAuthor, newAuthor)
+			exitCode = 1
+		}
+	})
+
+	c.OnHTML("meta[name=sessionnet]", func(e *colly.HTMLElement) {
+		newVersion := e.Attr("content")
+
+		if newVersion != expectedVersion {
+			fmt.Printf("Version changed (from/to)\n%s\n%s\n", expectedVersion, newVersion)
+			exitCode = 1
+		}
+	})
+
+	c.OnError(func(_ *colly.Response, err error) {
+		fmt.Println("Something went wrong:", err)
+	})
+
+	c.OnScraped(func(_ *colly.Response) {
+		os.Exit(exitCode)
+	})
+
+	c.Visit(url)
+}
+
+func main() {
+	checkVersion()
+}
